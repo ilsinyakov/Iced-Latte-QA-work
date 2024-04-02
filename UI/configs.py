7bf577f889ef88084d9773e4228d6cadc41c65d5
ilsinyakov <140279745+ilsinyakov@users.noreply.github.com>
2024-04-02 16:51:19 +0300

    Change new first name 2-3-127-128

--- UI/configs.py
+++ UI/configs.py
@@ -3,6 +3,6 @@ first_name = "Test"
 last_name = "Testtest"
 email = "ilsinyakovmo4@gmail.com"
 password = "80000005a"
-new_first_name_positive = ["Il","ily", "Ilya "]
+new_first_name_positive = ["Il","ily", "vfmpevqkeukeukidxtyxWSqzhdpstrahexbgtmypsmmtqv-ebupnzrslccysaduwtgncthlraykruyqcaqchoelsfiktzvdaewbwymapkfmwxfgbrqqwrsdhoamiyto", "vfmpevqkeukeukidxtyxwsqzhdpstrahexbgtmypsmmtqvfebupnzrslccys duwtgncthlraykruyqcaqchoelsfiktzvdaewbwymapkfmwxfgbrqqwrsdhoamiytob"]
 new_last_name = "Sinyakov"
 new_email = "new@gmail.com"
