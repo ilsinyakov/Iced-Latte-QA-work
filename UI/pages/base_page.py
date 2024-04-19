2f719491211c4ea589e62d8321101e8a2b9d7ac7
ilsinyakov <140279745+ilsinyakov@users.noreply.github.com>
2024-04-19 13:42:12 +0300

    script

--- UI/pages/base_page.py
+++ UI/pages/base_page.py
@@ -29,10 +29,17 @@ class BasePage:
     # check that sort drop-down is not present on the page
     def is_dropdown_not_present(self):
         dropdown = self.browser.find_element(*BasePageLocators.SORT_DROPDOWN)
-        if dropdown.is_displayed():
-            return False
-        else:
+        script = "return arguments[0].offsetWidth === 0 && arguments[0].offsetHeight === 0;"
+        if self.browser.execute_script(script, dropdown):
             return True
+        else:
+            return False
+        
+    '''element = driver.find_element_by_css_selector('selector')
+    script = "return arguments[0].offsetWidth === 0 && arguments[0].offsetHeight === 0;"
+    is_invisible = driver.execute_script(script, element)
+    print("Элемент {}".format("невидим" if is_invisible else "видим"))
+    '''
     
     # open page
     def open(self):
