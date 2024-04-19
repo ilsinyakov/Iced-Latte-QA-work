1bfdbd602a27537c32f187fa37797fd06fb976b8
ilsinyakov <140279745+ilsinyakov@users.noreply.github.com>
2024-04-19 10:31:34 +0300

    fix3

--- UI/test_dropdawn_not_present.py
+++ UI/test_dropdawn_not_present.py
@@ -21,9 +21,10 @@ def test_dropdawn_not_present(browser):
     with step('Assert Sort Drop-down is not present on Login Page'):
         page = LoginPage(browser, browser.current_url)
         assert page.is_dropdown_not_present(), 'Sort drop-down is present on Login Page'
+        sleep(2)
     with step('Go to Registration Page'):
         page.go_to_registration_page()
     with step('Assert Sort Drop-down is not present on Registration Page'):
         page = RegistrationPage(browser, browser.current_url)
         assert page.is_dropdown_not_present(), 'Sort drop-down is present on Registration Page'
-    sleep(2)
+        sleep(2)
