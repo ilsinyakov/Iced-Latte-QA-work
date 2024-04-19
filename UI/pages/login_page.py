538c51798517429f77d66de070599e837c7150f2
ilsinyakov <140279745+ilsinyakov@users.noreply.github.com>
2024-04-19 15:04:00 +0300

    fix13

--- UI/pages/login_page.py
+++ UI/pages/login_page.py
@@ -8,15 +8,7 @@ class LoginPage(BasePage):
     def go_to_registration_page(self):
         link = self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON)
         link.click()
-
-    def login_existing_user(self, email, password):
-        email_field = self.browser.find_element(*LoginPageLocators.EMAIL_FIELD)
-        password_field = self.browser.find_element(*LoginPageLocators.PASSWORD_FIELD)
-        login_button = self.browser.find_element(*LoginPageLocators.LOGIN_BUTTON)
-        email_field.send_keys(email)
-        password_field.send_keys(password)
-        login_button.click()
-
+    
     # check that sort drop-down is not present on the page
     def is_dropdown_present(self):
         try:
@@ -24,3 +16,11 @@ class LoginPage(BasePage):
         except NoSuchElementException:
             return False
         return True        
+
+    def login_existing_user(self, email, password):
+        email_field = self.browser.find_element(*LoginPageLocators.EMAIL_FIELD)
+        password_field = self.browser.find_element(*LoginPageLocators.PASSWORD_FIELD)
+        login_button = self.browser.find_element(*LoginPageLocators.LOGIN_BUTTON)
+        email_field.send_keys(email)
+        password_field.send_keys(password)
+        login_button.click()    
