1a8f9d79c0e7c77278e82ed7e91786f71c68e745
ilsinyakov <140279745+ilsinyakov@users.noreply.github.com>
2024-04-22 09:56:17 +0300

    fix2

--- UI/pages/cart_page.py
+++ UI/pages/cart_page.py
@@ -1,4 +1,6 @@
+from .base_page import BasePage
 from .locators import CartPageLocators
+
 
 class CartPage(BasePage):
     def is_cart_empty(self):
@@ -7,3 +9,4 @@ class CartPage(BasePage):
     def go_to_main_page(self):
         button = self.browser.find_element(*CartPageLocators.CONTINUE_SHOPPING_BUTTON)
         button.click()
+        
