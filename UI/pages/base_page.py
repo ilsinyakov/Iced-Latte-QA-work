f3c9696e944a8a43f7159d8cb5a0f6de3b5acfa8
ilsinyakov <140279745+ilsinyakov@users.noreply.github.com>
2024-04-26 08:30:43 +0300

    timeout 4

--- UI/pages/base_page.py
+++ UI/pages/base_page.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 class BasePage:    
 
-    def __init__(self, browser, url, timeout=2):
+    def __init__(self, browser, url, timeout=4):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)  # turn on implicitly wait   
