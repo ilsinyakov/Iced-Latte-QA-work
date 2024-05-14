7f9577598090bcdc6bdfa09fc8c2382e6706c374
ilsinyakov <140279745+ilsinyakov@users.noreply.github.com>
2024-05-14 16:39:19 +0300

    set

--- UI/set_of_steps.py
+++ UI/set_of_steps.py
@@ -1,6 +1,9 @@
 from .pages.base_page import BasePage
+from .pages.cart_page import CartPage
+from .pages.favorites_page import FavoritesPage
 from .pages.login_page import LoginPage
 from .pages.profile_page import ProfilePage
+
 from .configs import email, password
 
 from allure import step
@@ -26,3 +29,10 @@ def go_to_edit_profile_page(browser, link):
     with step('Click "Edit" button'):
         page = ProfilePage(browser, browser.current_url)
         page.go_to_edit_page()
+
+def remove_products_from_cart_and_fovorites(browser, link):
+    with step('Go to cart page'):
+        page = BasePage(browser, link)
+        page.go_to_cart_page()
+    
+    
