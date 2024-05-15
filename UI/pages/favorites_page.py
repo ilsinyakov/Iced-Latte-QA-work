edfb894e90008858fa363debe42c0468c03929df
ilsinyakov <140279745+ilsinyakov@users.noreply.github.com>
2024-05-15 16:49:08 +0300

    set3

---
+++ UI/pages/favorites_page.py
@@ -0,0 +1,17 @@
+from .base_page import BasePage
+from .locators import FavoritesPageLocators, HeaderLocators
+
+class FavoritesPage(BasePage):
+    def go_to_product_page(self):
+        link = self.browser.find_element(*FavoritesPageLocators.PRODUCT_LINK)
+        link.click()
+
+    def is_product_in_favorites(self, product_name):
+        favorites_page_product_name = self.browser.find_element(*FavoritesPageLocators.PRODUCT_NAME)
+        if favorites_page_product_name.text == product_name:
+            return True
+        else: 
+            return False
+    
+    def remove_favorites_products():
+        buttons = self.browser.find_elements(*FavoritesPageLocators.UNLIKE_BUTTON)
