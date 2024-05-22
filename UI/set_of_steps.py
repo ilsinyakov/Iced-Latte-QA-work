dafe170e35eff1ce8da13e1587f10b3e7cb517fb
ilsinyakov <140279745+ilsinyakov@users.noreply.github.com>
2024-05-22 11:53:01 +0300

    fix3

--- UI/set_of_steps.py
+++ UI/set_of_steps.py
@@ -19,7 +19,7 @@ def login_user(browser, link):
     with step('Login existing user'):
         login_page = LoginPage(browser, browser.current_url)
         login_page.login_existing_user(email, password)
-        sleep(3)
+        sleep(2)
 
 
 def go_to_edit_profile_page(browser, link):    
