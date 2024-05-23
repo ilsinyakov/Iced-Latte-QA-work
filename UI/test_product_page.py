d885e46372f582591bcbbfb0fe23437bdc4cada9
ilsinyakov <140279745+ilsinyakov@users.noreply.github.com>
2024-05-23 09:30:17 +0300

    skip

--- UI/test_product_page.py
+++ UI/test_product_page.py
@@ -57,7 +57,7 @@ class TestPruductPage:
             'Product reviews count is not equal on main and product pages'
 
     @title('Test header links. User is not logged-in')
-    @pytest.mark.skip()
+    # @pytest.mark.skip()
     def test_header_links_guest(self, browser):
         with step('Open main page'):            
             page = BasePage(browser, link)
@@ -81,7 +81,7 @@ class TestPruductPage:
             assert page.is_cart_page_link_clickable(), 'Cart page link is not clickable'
     
     @title('Add product to cart and change quantity. User is not logged-in')
-    @pytest.mark.skip()
+    # @pytest.mark.skip()
     def test_add_to_cart_guest(self, browser):
         with step('Open main page'):            
             page = BasePage(browser, link)
@@ -111,7 +111,7 @@ class TestPruductPage:
             assert page.is_product_in_cart(product_page_product_name), 'There is not product in the cart'
 
     @title('Add product to favorires. User is not logged-in')
-    @pytest.mark.skip()
+    # @pytest.mark.skip()
     def test_add_to_favorites_guest(self, browser):
         with step('Open main page'):            
             page = BasePage(browser, link)
@@ -123,13 +123,11 @@ class TestPruductPage:
             page = ProductPage(browser, browser.current_url)
             product_page_product_name = page.get_product_name()
         with step('Add product to favorires'):
-            page.add_product_to_favorites()
-            sleep(3)
+            page.add_product_to_favorites()            
             assert page.is_heart_red(), 'Heart button is not red'
             assert page.is_header_heart_blue(), 'Header favorites link is not blue'
         with step('Check product in the favorites list'):
-            page.go_to_favorites_page()
-            sleep(3)
+            page.go_to_favorites_page()            
             page = FavoritesPage(browser, browser.current_url)
             assert page.is_product_in_favorites(product_page_product_name), \
                                     'There is not product in favorites list'
@@ -138,8 +136,7 @@ class TestPruductPage:
             sleep(2) # waiting is mandatory (do not remove)
             page = ProductPage(browser, browser.current_url)
         with step('Remove product from favorites'):
-            page.remove_product_from_favorites()
-            sleep(3)
+            page.remove_product_from_favorites()            
             assert page.is_heart_transparent(), 'Heart button is not transparent'
             assert page.is_header_heart_black(), 'Header favorites link is not black'
 
@@ -187,7 +184,7 @@ class TestPruductPage:
             'Product reviews count is not equal on main and product pages'
 
     @title('Test header links. User is logged-in')
-    @pytest.mark.skip()
+    # @pytest.mark.skip()
     def test_header_links_user(self, browser):
         with step('Login User'):            
             login_user(browser, link)
@@ -211,7 +208,7 @@ class TestPruductPage:
             assert page.is_cart_page_link_clickable(), 'Cart page link is not clickable'
 
     @title('Add product to cart and change quantity. User is logged-in')
-    @pytest.mark.skip()
+    # @pytest.mark.skip()
     def test_add_to_cart_user(self, browser):    
         with step('Login user'):            
             login_user(browser, link)            
@@ -242,7 +239,7 @@ class TestPruductPage:
             assert page.is_product_in_cart(product_page_product_name), 'There is not product in the cart'
     
     @title('Add product to favorires. User is logged-in')
-    @pytest.mark.skip()
+    # @pytest.mark.skip()
     def test_add_to_favorites_user(self, browser):
         with step('Login user'):            
             login_user(browser, link)
