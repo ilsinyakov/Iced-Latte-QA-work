d557079b94fc970c76d1fbb5d4204a49b5f4a6b7
ilsinyakov <140279745+ilsinyakov@users.noreply.github.com>
2024-05-23 15:32:35 +0300

    - go main page

--- UI/test_change_email_negative.py
+++ UI/test_change_email_negative.py
@@ -22,10 +22,10 @@ def test_user_cant_change_email(browser, new_email):
         page.change_email(new_email)
     with step('Click "Save Changes" Button'):
         page.save_change()        
-    with step('Go to Main Page'):        
-        page.go_to_main_page()
+    # with step('Go to Main Page'):        
+    #     page.go_to_main_page()
     with step('Go to Profile Page'):
-        page = BasePage(browser, browser.current_url)
+        # page = BasePage(browser, link)
         page.go_to_profile_page()        
     with step('Assert New Negative Email is not Present in Profile'):
         page = ProfilePage(browser, browser.current_url)
