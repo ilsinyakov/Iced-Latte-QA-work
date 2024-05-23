d70843016b97c8cfbbde482a3877a40f16acd223
ilsinyakov <140279745+ilsinyakov@users.noreply.github.com>
2024-05-23 16:18:25 +0300

    fix14

--- UI/test_change_email_negative.py
+++ UI/test_change_email_negative.py
@@ -17,20 +17,17 @@ from time import sleep
 @pytest.mark.parametrize('new_email', new_email_negative)
 def test_user_cant_change_email(browser, new_email):
     with step('Go to Edit Profile Page'):        
-        go_to_edit_profile_page(browser, link)
-        sleep(2)
+        go_to_edit_profile_page(browser, link)        
     with step('Enter new Negative Email'):
         page = EditProfilePage(browser, browser.current_url)
         page.change_email(new_email)
     with step('Click "Save Changes" Button'):
         page.save_change()        
     with step('Go to Main Page'):        
-        page.go_to_main_page()
-        sleep(2)
+        page.go_to_main_page()        
     with step('Go to Profile Page'):
         page = BasePage(browser, link)        
-        page.go_to_profile_page()        
-        sleep(2)
+        page.go_to_profile_page()                
     with step('Assert New Negative Email is not Present in Profile'):
         page = ProfilePage(browser, browser.current_url)
         assert not page.is_new_email_present(new_email), 'New Negative Email is Present in Profile'
