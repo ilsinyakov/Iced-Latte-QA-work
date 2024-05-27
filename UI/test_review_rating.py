6bf3de03f791b60ab01a4ba74aed6fbe218fd93e
ilsinyakov <140279745+ilsinyakov@users.noreply.github.com>
2024-05-27 10:10:46 +0300

    review4

---
+++ UI/test_review_rating.py
@@ -0,0 +1,29 @@
+from allure import step, title, severity, story, severity_level
+from time import sleep
+
+from .pages.base_page import BasePage
+from .pages.favorites_page import FavoritesPage
+from .pages.product_page import ProductPage
+from .pages.cart_page import CartPage
+from .set_of_steps import login_user, remove_products_from_cart_and_favorites
+from .configs import link
+
+
+@story("Review, Rating")
+# @allure.description("")
+# @allure.tag("")
+@severity(severity_level.NORMAL)
+class TestReviewRating:
+    def test_add_review_guest(self, browser):
+        with step('Open main page'):            
+                page = BasePage(browser, link)
+                page.open()
+                sleep(2)  # waiting is mandatory (do not remove)
+        with step('Go to product page'):
+            page.go_to_product_page()
+            sleep(2)  # waiting is mandatory (do not remove)
+            page = ProductPage(browser, browser.current_url)
+        with step('Push "Add a review button"'):
+            page.add_review()
+            assert browser.current_url == 'https://iced-latte.uk/auth/login', \
+                'The guest was not redirected to the login page'
