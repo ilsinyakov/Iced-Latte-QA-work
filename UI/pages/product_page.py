b9c98912bcd96b205ba26abfe9a3fcb93aaf7cc1
ilsinyakov <140279745+ilsinyakov@users.noreply.github.com>
2024-06-10 10:41:40 +0300

    fix6

--- UI/pages/product_page.py
+++ UI/pages/product_page.py
@@ -214,6 +214,7 @@ class ProductPage(BasePage):
         filtered_ratings = self.browser.find_elements(*ProductPageLocators.RATINGS_IN_REVIEWS_LIST)
         
         for filtered_rating in filtered_ratings:
+            print('filtered_rating', filtered_rating)
             if filtered_rating.text not in rating:
                 is_only_filtered_ratings = False
         
